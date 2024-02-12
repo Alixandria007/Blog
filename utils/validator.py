b0260e08ed6753e1cab90433b234ca2230dd89fc
Alixandria007 <juninhokrauts778@gmail.com>
2024-02-12 14:41:27 -0300

    Adicionei o css remedy

--- utils/validator.py
+++ utils/validator.py
@@ -2,5 +2,5 @@ from django.core.exceptions import ValidationError
 
 
 def validate_png(image):
-    if not image.lower().endwith('.png'):
+    if not image.name.lower().endswith('.png'):
         raise ValidationError('Envie uma imagem png')
