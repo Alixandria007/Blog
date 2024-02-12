85845ff4e2878f518cf16def8b261a79ec044f6b
Alixandria007 <juninhokrauts778@gmail.com>
2024-02-12 13:29:39 -0300

    Model Site Setup incluido

--- site_setup/admin.py
+++ site_setup/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.http import HttpRequest
 from site_setup import models
 
 # Register your models here.
@@ -8,4 +9,20 @@ class MenuLinkAdmin(admin.ModelAdmin):
     list_display = 'id','text','url_or_file','new_tab'
     ordering = '-id',
     list_editable = 'new_tab',
+    list_per_page = 10
+    list_max_show_all = 200
     list_display_links = 'id',
+
+class MenuLinkInline(admin.TabularInline):
+    model = models.MenuLink
+    extra = 1
+    
+
+@admin.register(models.SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display = 'id', 'title', 'description'
+    list_display_links = 'id',
+    inlines = MenuLinkInline , 
+
+    def has_add_permission(self, request):
+        return not models.SiteSetup.objects.exists()
