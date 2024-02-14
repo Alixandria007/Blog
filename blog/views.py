d4ef4dc27a72eddef6968fb52decc8b356617931
Alixandria007 <juninhokrauts778@gmail.com>
2024-02-14 20:00:57 -0300

    Adicionei um paginator ao contexto da minha view Index

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/pages/index.html')
+    posts = 1000
+
+    paginator = Paginator(posts,10)
+    page_number = paginator.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+    }
+
+
+    return render(request, 'blog/pages/index.html', context)
 
 def post(request):
     return render(request, 'blog/pages/post.html')
