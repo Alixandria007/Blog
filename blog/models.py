a99a992c7e60bf1bb0bcb6f48802e115f5c2d095
Alixandria007 <juninhokrauts778@gmail.com>
2024-02-15 19:58:40 -0300

    Criei o model Tag

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+
+    name = models.CharField(
+        max_length = 255
+    )
+    slug = models.SlugField(
+        unique = True, blank = False, max_length = 255
+    )
+
+    def __str__(self) -> str:
+        return self.name
