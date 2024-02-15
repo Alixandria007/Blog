58248db8343826066ac4d3b778172f911cd07d1b
Alixandria007 <juninhokrauts778@gmail.com>
2024-02-15 20:07:08 -0300

    Criei o admin do meu model Tag

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from blog.models import Tag
 
 # Register your models here.
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = 'id', 'name', 'slug'
+    list_display_links = 'id',
+    prepopulated_fields = {
+        "slug": ('name',),
+    }
